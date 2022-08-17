e815f4c0e0e7e8c17b5b62b921feafd62dab1a8a
yangliping <yangliping18@huawei.com>
2022-08-17 15:06:21 +0800

    benchmarktest repeat
    Signed-off-by: yangliping <yangliping18@huawei.com>

--- test/benchmarktest/audiorenderer/benchmark_audiorenderer_test.cpp
+++ test/benchmarktest/audiorenderer/benchmark_audiorenderer_test.cpp
@@ -16,7 +16,9 @@
 #include <benchmark/benchmark.h>
 #include <string>
 #include <vector>
-
+#include <chrono>
+#include <thread>
+#include "audio_errors.h"
 #include "audio_info.h"
 #include "audio_renderer.h"
 
@@ -24,107 +26,196 @@ using namespace std;
 using namespace OHOS;
 using namespace OHOS::AudioStandard;
 
-namespace {
-class AudioRendererTest : public benchmark::Fixture {
-public:
-    AudioRendererTest()
-    {
-        Iterations(iterations);
-        Repetitions(repetitions);
-        ReportAggregatesOnly();
-    }
-
-    ~AudioRendererTest() override = default;
-
-    void SetUp(const ::benchmark::State &state) override
-    {
-        sleep(1);
-        AudioRendererOptions rendererOptions;
-        rendererOptions.streamInfo.samplingRate = AudioSamplingRate::SAMPLE_RATE_96000;
-        rendererOptions.streamInfo.encoding = AudioEncodingType::ENCODING_PCM;
-        rendererOptions.streamInfo.format = AudioSampleFormat::SAMPLE_U8;
-        rendererOptions.streamInfo.channels = AudioChannel::MONO;
-        rendererOptions.rendererInfo.contentType = ContentType::CONTENT_TYPE_MUSIC;
-        rendererOptions.rendererInfo.streamUsage = StreamUsage::STREAM_USAGE_MEDIA;
-        rendererOptions.rendererInfo.rendererFlags = RENDERER_FLAG;
-        audioRenderer = AudioRenderer::Create(rendererOptions);
-        sleep(1);
-    }
-
-    void TearDown(const ::benchmark::State &state) override
-    {
-    }
-
-protected:
-    unique_ptr<AudioRenderer> audioRenderer;
-    const string AUDIORENDER_TEST_FILE_PATH = "/data/test_44100_2.wav";
-    const int32_t RENDERER_FLAG = 0;
-    const int32_t repetitions = 16;
-    const int32_t iterations = 1;
-};
-
-// StartAbility
-BENCHMARK_F(AudioRendererTest, StartAbilityTestCase)(
-    benchmark::State &state)
+namespace
 {
-    while (state.KeepRunning()) {
-         bool isStarted = audioRenderer->Start();
-         if(isStarted==false){
-            state.SkipWithError("StartAbilityTestCase audioRenderer start failed.");
-         }
+    class AudioRendererTest : public benchmark::Fixture
+    {
+    public:
+        AudioRendererTest()
+        {
+            Iterations(iterations);
+            Repetitions(repetitions);
+            ReportAggregatesOnly();
+        }
 
-         state.PauseTiming();
-         bool isStopped = audioRenderer->Stop();
-         if(isStopped==false){
-            state.SkipWithError("StartAbilityTestCase audioRenderer start failed.");
-         }
-         state.ResumeTiming();
+        ~AudioRendererTest() override = default;
+
+        void SetUp(const ::benchmark::State &state) override
+        {
+            sleep(1);
+            AudioRendererOptions rendererOptions;
+            rendererOptions.streamInfo.samplingRate = AudioSamplingRate::SAMPLE_RATE_44100;
+            rendererOptions.streamInfo.encoding = AudioEncodingType::ENCODING_PCM;
+            rendererOptions.streamInfo.format = AudioSampleFormat::SAMPLE_S16LE;
+            rendererOptions.streamInfo.channels = AudioChannel::STEREO;
+            rendererOptions.rendererInfo.contentType = ContentType::CONTENT_TYPE_MUSIC;
+            rendererOptions.rendererInfo.streamUsage = StreamUsage::STREAM_USAGE_MEDIA;
+            rendererOptions.rendererInfo.rendererFlags = RENDERER_FLAG;
+            audioRenderer = AudioRenderer::Create(rendererOptions);
+            sleep(1);
+        }
+
+        void TearDown(const ::benchmark::State &state) override
+        {
+            audioRenderer->Release();
+        }
+
+    protected:
+        unique_ptr<AudioRenderer> audioRenderer;
+        const int32_t RENDERER_FLAG = 0;
+        const int32_t repetitions = 3;
+        const int32_t iterations = 300;
+        const string AUDIORENDER_TEST_FILE_PATH = "/data/test_44100_2.wav";
+    };
+
+    // StartAbility
+    BENCHMARK_F(AudioRendererTest, StartAbilityTestCase)
+    (
+        benchmark::State &state)
+    {
+        while (state.KeepRunning())
+        {
+            bool isStarted = audioRenderer->Start();
+            if (isStarted == false)
+            {
+                state.SkipWithError("StartAbilityTestCase audioRenderer start failed.");
+            }
+
+            state.PauseTiming();
+            bool isStopped = audioRenderer->Stop();
+            if (isStopped == false)
+            {
+                state.SkipWithError("StartAbilityTestCase audioRenderer start failed.");
+            }
+            state.ResumeTiming();
+        }
     }
-}
 
-// PauseAbility
-BENCHMARK_F(AudioRendererTest, PauseAbilityTestCase)(
-    benchmark::State &state)
-{
-    while (state.KeepRunning()) {
-         state.PauseTiming();
-         bool isStarted = audioRenderer->Start();
-         if(isStarted==false){
-            state.SkipWithError("PauseAbilityTestCase audioRenderer start failed.");
-         }
-         
-        bool isPaused = audioRenderer->Pause();
-        if(isPaused==false){
-            state.SkipWithError("PauseAbilityTestCase audioRenderer pause failed.");
-         } 
+    // PauseAbility
+    BENCHMARK_F(AudioRendererTest, PauseAbilityTestCase)
+    (
+        benchmark::State &state)
+    {
+        while (state.KeepRunning())
+        {
+            state.PauseTiming();
+            bool isStarted = audioRenderer->Start();
+            if (isStarted == false)
+            {
+                state.SkipWithError("PauseAbilityTestCase audioRenderer start failed.");
+            }
 
-         state.PauseTiming();
-         bool isStopped = audioRenderer->Stop();
-         if(isStopped==false){
-            state.SkipWithError("PauseAbilityTestCase audioRenderer start failed.");
-         }  
-         state.ResumeTiming();
+            bool isPaused = audioRenderer->Pause();
+            if (isPaused == false)
+            {
+                state.SkipWithError("PauseAbilityTestCase audioRenderer pause failed.");
+            }
+
+            state.PauseTiming();
+            bool isStopped = audioRenderer->Stop();
+            if (isStopped == false)
+            {
+                state.SkipWithError("PauseAbilityTestCase audioRenderer start failed.");
+            }
+            state.ResumeTiming();
+        }
     }
-}
 
-// StopAbility
-BENCHMARK_F(AudioRendererTest, StopAbilityTestCase)(
-    benchmark::State &state)
-{
-    while (state.KeepRunning()) {
-         state.PauseTiming();
-         bool isStarted = audioRenderer->Start();
-         if(isStarted==false){
-            state.SkipWithError("StopAbilityTestCase audioRenderer start failed.");
-         }
-         state.ResumeTiming();
+    // StopAbility
+    BENCHMARK_F(AudioRendererTest, StopAbilityTestCase)
+    (
+        benchmark::State &state)
+    {
+        while (state.KeepRunning())
+        {
+            state.PauseTiming();
+            bool isStarted = audioRenderer->Start();
+            if (isStarted == false)
+            {
+                state.SkipWithError("StopAbilityTestCase audioRenderer start failed.");
+            }
+            state.ResumeTiming();
 
-         bool isStopped = audioRenderer->Stop();
-         if(isStopped==false){
-            state.SkipWithError("StopAbilityTestCase audioRenderer start failed.");
-         }  
+            bool isStopped = audioRenderer->Stop();
+            if (isStopped == false)
+            {
+                state.SkipWithError("StopAbilityTestCase audioRenderer start failed.");
+            }
+        }
     }
-}
+
+    // SetVolumeAbility
+    BENCHMARK_F(AudioRendererTest, SetVolumeAbilityTestCase)
+    (
+        benchmark::State &state)
+    {
+        while (state.KeepRunning())
+        {
+            state.PauseTiming();
+            int32_t ret = -1;
+            ret = audioRenderer->SetVolume(0);
+            if (ret != SUCCESS)
+            {
+                state.SkipWithError("SetVolumeAbilityTestCase 0 audioRenderer failed.");
+            }
+            state.ResumeTiming();
+
+            ret = audioRenderer->SetVolume(1.0);
+            if (ret != SUCCESS)
+            {
+                state.SkipWithError("SetVolumeAbilityTestCase 1.0 audioRenderer failed.");
+            }
+        }
+    }
+
+    // WriteAbility  ****************** write
+    BENCHMARK_F(AudioRendererTest, WriteAbilityTestCase)
+    (
+        benchmark::State &state)
+    {
+        while (state.KeepRunning())
+        {
+            state.PauseTiming();
+            int32_t ret = -1;
+
+            FILE *wavFile = fopen(AUDIORENDER_TEST_FILE_PATH.c_str(), "rb");
+            if (wavFile == nullptr)
+            {
+                state.SkipWithError("WriteAbilityTestCase fopen fail");
+            }
+
+            bool isStarted = audioRenderer->Start();
+            if (isStarted == false)
+            {
+                state.SkipWithError("WriteAbilityTestCase audioRenderer start fail");
+            }
+
+            size_t bufferLen;
+            ret = audioRenderer->GetBufferSize(bufferLen);
+
+            uint8_t *buffer = (uint8_t *)malloc(bufferLen);
+            if (buffer == nullptr)
+            {
+                state.SkipWithError("WriteAbilityTestCase getbuffer fail");
+            }
+            size_t bytesToWrite = 0;
+            int32_t bytesWritten = 0;
+
+            bytesToWrite = fread(buffer, 1, bufferLen, wavFile);
+            state.ResumeTiming();
+
+            bytesWritten = audioRenderer->Write(buffer, bytesToWrite);
+            if (bytesWritten <= 0)
+            {
+                state.SkipWithError("WriteAbilityTestCase Write fail");
+            }
+            state.PauseTiming();
+            audioRenderer->Stop();
+            free(buffer);
+            fclose(wavFile);
+            state.ResumeTiming();
+        }
+    }
 }
 
 // Run the benchmark
