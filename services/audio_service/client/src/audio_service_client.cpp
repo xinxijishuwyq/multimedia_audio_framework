cd2b21673a1871a6cb2004c7adb807f0cbf7f511
magekkkk <lixin113@huawei.com>
2022-09-11 07:56:09 +0000

    refactor callback mode renderer to fix glitch
    Signed-off-by: magekkkk <lixin113@huawei.com>

--- services/audio_service/client/src/audio_service_client.cpp
+++ services/audio_service/client/src/audio_service_client.cpp
@@ -333,21 +333,6 @@ void AudioServiceClient::PAStreamWriteCb(pa_stream *stream, size_t length, void 
     asClient->mWriteCbStamp = now;
     auto mainLoop = static_cast<pa_threaded_mainloop *>(asClient->mainLoop);
     pa_threaded_mainloop_signal(mainLoop, 0);
-
-    if (asClient->renderMode_ != RENDER_MODE_CALLBACK) {
-        return;
-    }
-
-    std::shared_ptr<AudioRendererWriteCallback> cb = asClient->writeCallback_.lock();
-    if (cb != nullptr) {
-        size_t requestSize;
-        asClient->GetMinimumBufferSize(requestSize);
-        AUDIO_INFO_LOG("AudioServiceClient::PAStreamWriteCb: cb != nullptr firing OnWriteData");
-        AUDIO_INFO_LOG("AudioServiceClient::OnWriteData requestSize : %{public}zu", requestSize);
-        cb->OnWriteData(requestSize);
-    } else {
-        AUDIO_ERR_LOG("AudioServiceClient::PAStreamWriteCb: cb == nullptr not firing OnWriteData");
-    }
 }
 
 void AudioServiceClient::PAStreamReadCb(pa_stream *stream, size_t length, void *userdata)
