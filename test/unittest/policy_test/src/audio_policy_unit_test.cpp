02c974a733cb54984e4eb48f2c4c19228399808c
yangliping <yangliping18@huawei.com>
2022-11-29 19:55:40 +0800

    test case
    Signed-off-by: yangliping <yangliping18@huawei.com>

--- test/unittest/policy_test/src/audio_policy_unit_test.cpp
+++ test/unittest/policy_test/src/audio_policy_unit_test.cpp
@@ -20,6 +20,10 @@
 #include "audio_system_manager.h"
 #include "iservice_registry.h"
 #include "system_ability_definition.h"
+#include "audio_capturer_state_change_listener_stub.h"
+#include "i_standard_renderer_state_change_listener.h"
+#include "i_standard_capturer_state_change_listener.h"
+#include "audio_renderer_state_change_listener_stub.h"
 
 using namespace std;
 using namespace testing::ext;
@@ -417,6 +421,219 @@ HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_IsAudioRendererLowLatencySuppor
     audioStreamInfo.channels = AudioChannel::MONO;
     bool ret = AudioPolicyManager::GetInstance().IsAudioRendererLowLatencySupported(audioStreamInfo);
     EXPECT_EQ(true, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_GetActiveOutputDeviceDescriptors_001 via legal state
+* @tc.number: Audio_Policy_GetActiveOutputDeviceDescriptors_001
+* @tc.desc  : Test GetActiveOutputDeviceDescriptors interface. Returns success.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_GetActiveOutputDeviceDescriptors_001, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    std::vector<sptr<AudioDeviceDescriptor>> deviceInfo;
+    deviceInfo = audioPolicyProxy->GetActiveOutputDeviceDescriptors();
+    EXPECT_EQ(true, deviceInfo.size() >= 0);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_SetMicStateChangeCallback_001 via legal state
+* @tc.number: Audio_Policy_SetMicStateChangeCallback_001
+* @tc.desc  : Test SetMicStateChangeCallback interface. Returns success.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_SetMicStateChangeCallback_001, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    int32_t clientId = getpid();
+    sptr<IRemoteObject> object;
+    AudioPolicyUnitTest::GetIRemoteObject(object);
+    int32_t ret = audioPolicyProxy->SetMicStateChangeCallback(clientId, object);
+    EXPECT_EQ(true, ret <= 0);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_SetMicStateChangeCallback_002 via illegal state
+* @tc.number: Audio_Policy_SetMicStateChangeCallback_002
+* @tc.desc  : Test SetMicStateChangeCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_SetMicStateChangeCallback_002, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    int32_t clientId = getpid();
+    sptr<IRemoteObject> object = nullptr;
+    int32_t ret = audioPolicyProxy->SetMicStateChangeCallback(clientId, object);
+    EXPECT_EQ(ERR_NULL_OBJECT, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_SetAudioInterruptCallback_001 via illegal state
+* @tc.number: Audio_Policy_SetAudioInterruptCallback_001
+* @tc.desc  : Test SetAudioInterruptCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_SetAudioInterruptCallback_001, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    AudioInterrupt audioInterrupt;
+    audioInterrupt.contentType = CONTENT_TYPE_RINGTONE;
+    audioInterrupt.streamUsage = STREAM_USAGE_NOTIFICATION_RINGTONE;
+    audioInterrupt.streamType = STREAM_ACCESSIBILITY;
+
+    std::shared_ptr<AudioStream> audioStream;
+    AudioPolicyUnitTest::InitAudioStream(audioStream);
+    ASSERT_NE(nullptr, audioStream);
+
+    uint32_t sessionID_ = AudioPolicyUnitTest::GetSessionId(audioStream);
+    sptr<IRemoteObject> object = nullptr;
+    int32_t ret = audioPolicyProxy->SetAudioInterruptCallback(sessionID_, object);
+    EXPECT_EQ(ERR_NULL_OBJECT, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_SetAudioManagerInterruptCallback_001 via illegal state
+* @tc.number: Audio_Policy_SetAudioManagerInterruptCallback_001
+* @tc.desc  : Test SetAudioManagerInterruptCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_SetAudioManagerInterruptCallback_001, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    int32_t clientId = getpid();
+    sptr<IRemoteObject> object = nullptr;
+    int32_t ret = audioPolicyProxy->SetAudioManagerInterruptCallback(clientId, object);
+    EXPECT_EQ(ERR_NULL_OBJECT, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_RegisterTracker_001 via illegal state
+* @tc.number: Audio_Policy_RegisterTracker_001
+* @tc.desc  : Test RegisterTracker interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_RegisterTracker_001, TestSize.Level1)
+{
+    std::shared_ptr<AudioPolicyProxy> audioPolicyProxy;
+    AudioPolicyUnitTest::InitAudioPolicyProxy(audioPolicyProxy);
+    ASSERT_NE(nullptr, audioPolicyProxy);
+
+    AudioMode mode = AudioMode::AUDIO_MODE_PLAYBACK;
+    AudioStreamChangeInfo streamChangeInfo;
+    sptr<IRemoteObject> object = nullptr;
+    int32_t ret = audioPolicyProxy->RegisterTracker(mode, streamChangeInfo, object);
+    EXPECT_EQ(ERR_NULL_OBJECT, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_SetRingerModeCallback_001 via illegal state
+* @tc.number: Audio_Policy_Manager_SetRingerModeCallback_001
+* @tc.desc  : Test SetRingerModeCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_SetRingerModeCallback_001, TestSize.Level1)
+{
+    int32_t clientId = getpid();
+    std::shared_ptr<AudioRingerModeCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().SetRingerModeCallback(clientId, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_SetDeviceChangeCallback_002 via illegal state
+* @tc.number: Audio_Policy_Manager_SetDeviceChangeCallback_002
+* @tc.desc  : Test SetDeviceChangeCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_SetDeviceChangeCallback_002, TestSize.Level1)
+{
+    int32_t clientId = getpid();
+    DeviceFlag flag = DeviceFlag::INPUT_DEVICES_FLAG;
+    std::shared_ptr<AudioManagerDeviceChangeCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().SetDeviceChangeCallback(clientId, flag, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_SetAudioInterruptCallback_001 via illegal state
+* @tc.number: Audio_Policy_Manager_SetAudioInterruptCallback_001
+* @tc.desc  : Test SetAudioInterruptCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_SetAudioInterruptCallback_001, TestSize.Level1)
+{
+    AudioInterrupt audioInterrupt;
+    audioInterrupt.contentType = CONTENT_TYPE_RINGTONE;
+    audioInterrupt.streamUsage = STREAM_USAGE_NOTIFICATION_RINGTONE;
+    audioInterrupt.streamType = STREAM_ACCESSIBILITY;
+
+    std::shared_ptr<AudioStream> audioStream;
+    AudioPolicyUnitTest::InitAudioStream(audioStream);
+    ASSERT_NE(nullptr, audioStream);
+
+    uint32_t sessionID_ = AudioPolicyUnitTest::GetSessionId(audioStream);
+    std::shared_ptr<AudioInterruptCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().SetAudioInterruptCallback(sessionID_, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_SetAudioManagerInterruptCallback_001 via illegal state
+* @tc.number: Audio_Policy_Manager_SetAudioManagerInterruptCallback_001
+* @tc.desc  : Test SetAudioManagerInterruptCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_SetAudioManagerInterruptCallback_001, TestSize.Level1)
+{
+    int32_t clientId = getpid();
+    std::shared_ptr<AudioInterruptCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().SetAudioManagerInterruptCallback(clientId, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_SetVolumeKeyEventCallback_001 via illegal state
+* @tc.number: Audio_Policy_Manager_SetVolumeKeyEventCallback_001
+* @tc.desc  : Test SetVolumeKeyEventCallback interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_SetVolumeKeyEventCallback_001, TestSize.Level1)
+{
+    int32_t clientPid = getpid();
+    std::shared_ptr<VolumeKeyEventCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().SetVolumeKeyEventCallback(clientPid, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_RegisterAudioRendererEventListener_002 via illegal state
+* @tc.number: Audio_Policy_Manager_RegisterAudioRendererEventListener_002
+* @tc.desc  : Test RegisterAudioRendererEventListener interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_RegisterAudioRendererEventListener_002, TestSize.Level1)
+{
+    int32_t clientUID = getpid();
+    std::shared_ptr<AudioRendererStateChangeCallback> callback = nullptr;
+    int32_t ret = AudioPolicyManager::GetInstance().RegisterAudioRendererEventListener(clientUID, callback);
+    EXPECT_EQ(ERR_INVALID_PARAM, ret);
+}
+
+/**
+* @tc.name  : Test Audio_Policy_Manager_GetCurrentCapturerChangeInfos_001 via illegal state
+* @tc.number: Audio_Policy_Manager_GetCurrentCapturerChangeInfos_001
+* @tc.desc  : Test GetCurrentCapturerChangeInfos interface. Returns invalid.
+*/
+HWTEST(AudioPolicyUnitTest, Audio_Policy_Manager_GetCurrentCapturerChangeInfos_001, TestSize.Level1)
+{
+    vector<unique_ptr<AudioCapturerChangeInfo>> audioCapturerChangeInfos;
+    int32_t ret = AudioPolicyManager::GetInstance().GetCurrentCapturerChangeInfos(audioCapturerChangeInfos);
+    EXPECT_EQ(true, audioCapturerChangeInfos.size() <= 0);
+    EXPECT_EQ(SUCCESS, ret);
 }
 } // namespace AudioStandard
 } // namespace OHOS
