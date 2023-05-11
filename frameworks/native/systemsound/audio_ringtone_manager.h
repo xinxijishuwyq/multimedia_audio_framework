0b6fad7236fa6a0164ede5468cdba43cb2f71dab
huang-xl <huangxiaolong1@huawei.com>
2023-05-11 10:02:28 +0800

    change player error Signed-off-by: huang-xl<huangxiaolong1@huawei.com>
    Signed-off-by: huang-xl <huangxiaolong1@huawei.com>
    Change-Id: I8cbddf4d6a371abea27d409ac3a05bc94a511952

--- frameworks/native/systemsound/audio_ringtone_manager.h
+++ frameworks/native/systemsound/audio_ringtone_manager.h
@@ -123,7 +123,7 @@ class RingtonePlayerCallback : public Media::PlayerCallback {
 public:
     explicit RingtonePlayerCallback(RingtonePlayer &ringtonePlayer);
     virtual ~RingtonePlayerCallback() = default;
-    void OnError(Media::PlayerErrorType errorType, int32_t errorCode) override;
+    void OnError(int32_t errorCode, const std::string &errorMsg) override;
     void OnInfo(Media::PlayerOnInfoType type, int32_t extra, const Media::Format &infoBody) override;
 
 private:
