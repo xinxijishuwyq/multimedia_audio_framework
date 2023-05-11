b090b1b736dfba33347eb169c32b094a67276be5
志强 <hezhiqiang19@huawei.com>
2023-05-11 12:00:44 +0000

    回退 'Pull Request !1426 : audio_policy中增加routermap的mutex'

--- services/audio_policy/server/include/service/audio_policy_service.h
+++ services/audio_policy/server/include/service/audio_policy_service.h
@@ -329,8 +329,6 @@ private:
 
     void SetEarpieceState();
 
-    void RemoveDeviceInRouterMap(std::string networkId);
-
     void UpdateDisplayName(sptr<AudioDeviceDescriptor> deviceDescriptor);
 
     void UpdateLocalGroupInfo(bool isConnected, const std::string& macAddress,
@@ -361,8 +359,6 @@ private:
     DeviceType currentActiveDevice_ = DEVICE_TYPE_NONE;
     DeviceType activeInputDevice_ = DEVICE_TYPE_NONE;
     DeviceType pnpDevice_ = DEVICE_TYPE_NONE;
-
-    std::mutex routerMapMutex_; // unordered_map is not concurrently-secure
     std::unordered_map<int32_t, std::pair<std::string, int32_t>> routerMap_;
     IAudioPolicyInterface& audioPolicyManager_;
     Parser& configParser_;
