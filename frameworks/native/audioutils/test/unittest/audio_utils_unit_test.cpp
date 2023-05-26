5b6d3cd44ff9460828a2dedd23483151690d4436
yangliping <yangliping25@huawei.com>
2023-05-26 09:41:46 +0800

    add audioutils unittest
    Signed-off-by: yangliping <yangliping25@huawei.com>

---
+++ frameworks/native/audioutils/test/unittest/audio_utils_unit_test.cpp
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "audio_utils.h"
+
+using namespace testing::ext;
+namespace OHOS {
+namespace AudioStandard {
+
+constexpr int32_t SUCCESS = 0;
+class AudioUtilsUnitTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void AudioUtilsUnitTest::SetUpTestCase(void)
+{
+    // input testsuit setup step，setup invoked before all testcases
+}
+
+void AudioUtilsUnitTest::TearDownTestCase(void)
+{
+    // input testsuit teardown step，teardown invoked after all testcases
+}
+
+void AudioUtilsUnitTest::SetUp(void)
+{
+    // input testcase setup step，setup invoked before each testcases
+}
+
+void AudioUtilsUnitTest::TearDown(void)
+{
+    // input testcase teardown step，teardown invoked after each testcases
+}
+
+/**
+* @tc.name  : Test ClockTime API
+* @tc.type  : FUNC
+* @tc.number: ClockTime_001
+* @tc.desc  : Test ClockTime interface.
+*/
+HWTEST(AudioUtilsUnitTest, ClockTime_001, TestSize.Level1)
+{
+    const int64_t CLOCK_TIME = 0;
+    int32_t ret = -1;
+    ret = ClockTime::AbsoluteSleep(CLOCK_TIME);
+    EXPECT_EQ(SUCCESS - 1, ret);
+
+    int64_t nanoTime = 1000;
+    ret = ClockTime::AbsoluteSleep(nanoTime);
+    EXPECT_EQ(SUCCESS, ret);
+
+    ret = ClockTime::RelativeSleep(CLOCK_TIME);
+    EXPECT_EQ(SUCCESS - 1, ret);
+
+    ret = ClockTime::RelativeSleep(nanoTime);
+    EXPECT_EQ(SUCCESS, ret);
+}
+
+/**
+* @tc.name  : Test Trace API
+* @tc.type  : FUNC
+* @tc.number: Trace_001
+* @tc.desc  : Test Trace interface.
+*/
+HWTEST(AudioUtilsUnitTest, Trace_001, TestSize.Level1)
+{
+    std::string value = "Test";
+    bool isShowLog = true;
+    bool isEnable = true;
+    std::shared_ptr<Trace> trace = std::make_shared<Trace>(value, isShowLog, isEnable);
+    trace->End();
+    int64_t count = 1;
+    Trace::Count(value, count, isEnable);
+    EXPECT_EQ(isEnable, isShowLog);
+}
+
+/**
+* @tc.name  : Test PermissionUtil API
+* @tc.type  : FUNC
+* @tc.number: PermissionUtil_001
+* @tc.desc  : Test PermissionUtil interface.
+*/
+HWTEST(AudioUtilsUnitTest, PermissionUtil_001, TestSize.Level1)
+{
+    bool ret1 = PermissionUtil::VerifyIsSystemApp();
+    EXPECT_EQ(false, ret1);
+    bool ret2 = PermissionUtil::VerifySelfPermission();
+    EXPECT_EQ(true, ret2);
+    bool ret3 = PermissionUtil::VerifySystemPermission();
+    EXPECT_EQ(true, ret3);
+}
+
+/**
+* @tc.name  : Test AdjustStereoToMonoForPCM API
+* @tc.type  : FUNC
+* @tc.number: AdjustStereoToMonoForPCM_001
+* @tc.desc  : Test AdjustStereoToMonoForPCM interface.
+*/
+HWTEST(AudioUtilsUnitTest, AdjustStereoToMonoForPCM_001, TestSize.Level1)
+{
+    uint64_t len = 2;
+
+    const int16_t Bit16RET = 1;
+    int16_t arr2[2] = {1, 2};
+    int16_t *data2 = &arr2[0];
+    AdjustStereoToMonoForPCM16Bit(data2, len);
+    EXPECT_EQ(Bit16RET, data2[0]);
+    EXPECT_EQ(Bit16RET, data2[1]);
+
+    const int8_t Bit8RET1 = 2;
+    const int8_t Bit8RET2 = 3;
+    const int8_t Bit8RET3 = 4;
+    int8_t arr3[6] = {1, 2, 3, 4, 5, 6};
+    int8_t *data3 = &arr3[0];
+    AdjustStereoToMonoForPCM24Bit(data3, len);
+    EXPECT_EQ(Bit8RET1, data3[0]);
+    EXPECT_EQ(Bit8RET2, data3[1]);
+    EXPECT_EQ(Bit8RET3, data3[2]);
+    EXPECT_EQ(Bit8RET1, data3[3]);
+    EXPECT_EQ(Bit8RET2, data3[4]);
+    EXPECT_EQ(Bit8RET3, data3[5]);
+
+    const int32_t Bit32RET = 1;
+    int32_t arr4[2] = {1, 2};
+    int32_t *data4 = &arr4[0];
+    AdjustStereoToMonoForPCM32Bit(data4, len);
+    EXPECT_EQ(Bit32RET, data4[0]);
+    EXPECT_EQ(Bit32RET, data4[1]);
+}
+
+/**
+* @tc.name  : Test AdjustAudioBalanceForPCM API
+* @tc.type  : FUNC
+* @tc.number: AdjustAudioBalanceForPCM_001
+* @tc.desc  : Test AdjustAudioBalanceForPCM interface.
+*/
+HWTEST(AudioUtilsUnitTest, AdjustAudioBalanceForPCM_001, TestSize.Level1)
+{
+    float left = 2.0;
+    float right = 2.0;
+    uint64_t len = 2;
+
+    const int8_t Bit8RET1 = 2;
+    const int8_t Bit8RET2 = 4;
+    int8_t arr1[2] = {1, 2};
+    int8_t *data1 = &arr1[0];
+    AdjustAudioBalanceForPCM8Bit(data1, len, left, right);
+    EXPECT_EQ(Bit8RET1, data1[0]);
+    EXPECT_EQ(Bit8RET2, data1[1]);
+
+    const int16_t Bit16RET1 = 2;
+    const int16_t Bit16RET2 = 4;
+    int16_t arr2[2] = {1, 2};
+    int16_t *data2 = &arr2[0];
+    AdjustAudioBalanceForPCM16Bit(data2, len, left, right);
+    EXPECT_EQ(Bit16RET1, data2[0]);
+    EXPECT_EQ(Bit16RET2, data2[1]);
+
+    int8_t arr3[6] = {1, 2, 3, 4, 5, 6};
+    int8_t *data3 = &arr3[0];
+    AdjustAudioBalanceForPCM24Bit(data3, len, left, right);
+    EXPECT_EQ(Bit8RET1, data3[0]);
+    EXPECT_EQ(Bit8RET1*2, data3[1]);
+    EXPECT_EQ(Bit8RET1*3, data3[2]);
+    EXPECT_EQ(Bit8RET1*4, data3[3]);
+    EXPECT_EQ(Bit8RET1*5, data3[4]);
+    EXPECT_EQ(Bit8RET1*6, data3[5]);
+
+    const int32_t Bit32RET = 2;
+    int32_t arr4[2] = {1, 2};
+    int32_t *data4 = &arr4[0];
+    AdjustAudioBalanceForPCM32Bit(data4, len, left, right);
+    EXPECT_EQ(Bit32RET, data4[0]);
+    EXPECT_EQ(Bit32RET*2, data4[1]);
+}
+} // namespace AudioStandard
+} // namespace OHOS
