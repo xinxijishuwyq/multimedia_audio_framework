7ae66ac8ebbcd11942769a7d7b8ced2df2ca78bd
songshenke <songshenke@huawei.com>
2023-06-27 22:10:15 +0800

    Add all volume types to vector for STREAM_ALL
    Signed-off-by: songshenke <songshenke@huawei.com>
    Change-Id: If2159f26dda2df6a81571b1540bde704affa046a

--- services/audio_policy/server/include/audio_policy_server.h
+++ services/audio_policy/server/include/audio_policy_server.h
@@ -51,10 +51,13 @@ public:
     };
 
     const std::vector<AudioStreamType> GET_STREAM_ALL_VOLUME_TYPES {
-        STREAM_MUSIC,
-        STREAM_RING,
         STREAM_VOICE_CALL,
-        STREAM_VOICE_ASSISTANT
+        STREAM_RING,
+        STREAM_MUSIC,
+        STREAM_VOICE_ASSISTANT,
+        STREAM_ALARM,
+        STREAM_ACCESSIBILITY,
+        STREAM_ULTRASONIC
     };
 
     explicit AudioPolicyServer(int32_t systemAbilityId, bool runOnCreate = true);
