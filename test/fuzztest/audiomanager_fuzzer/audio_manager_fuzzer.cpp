0e3ee1928e50d7f3d4472700372e29aec8c55ad5
wangzx0705 <wangzx0705@thundersoft.com>
2023-06-29 05:13:26 +0000

    modify the fuzztest casename
    Signed-off-by: wangzx0705 <wangzx0705@thundersoft.com>

--- test/fuzztest/audiomanager_fuzzer/audio_manager_fuzzer.cpp
+++ test/fuzztest/audiomanager_fuzzer/audio_manager_fuzzer.cpp
@@ -35,7 +35,7 @@ void AudioRendererStateCallbackFuzz::OnRendererStateChange(
 void AudioCapturerStateCallbackFuzz::OnCapturerStateChange(
     const std::vector<std::unique_ptr<AudioCapturerChangeInfo>> &audioCapturerChangeInfos) {}
 const int32_t LIMITSIZE = 4;
-void AudioManagerFuzzTestFramework(const uint8_t* data, size_t size)
+void AudioManagerFuzzTest(const uint8_t* data, size_t size)
 {
     if ((data == nullptr) || (size < LIMITSIZE)) {
         return;
@@ -144,7 +144,7 @@ void AudioGroupManagerFuzzTest(const uint8_t* data, size_t size)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
-    OHOS::AudioStandard::AudioManagerFuzzTestFramework(data, size);
+    OHOS::AudioStandard::AudioManagerFuzzTest(data, size);
     OHOS::AudioStandard::AudioRoutingManagerFuzzTest(data, size);
     OHOS::AudioStandard::AudioStreamManagerFuzzTest(data, size);
     OHOS::AudioStandard::AudioGroupManagerFuzzTest(data, size);
