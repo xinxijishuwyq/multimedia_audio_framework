15b437f96a8a8c29783d394b88145e335d336c3e
kw <wangyuqi30@huawei.com>
2023-11-14 11:00:40 +0800

    adjust the key of mmi and perf_info to align with HDI
    Signed-off-by: kw <wangyuqi30@huawei.com>

--- interfaces/inner_api/native/audiocommon/include/audio_info.h
+++ interfaces/inner_api/native/audiocommon/include/audio_info.h
@@ -724,8 +724,8 @@ enum AudioParamKey {
     A2DP_SUSPEND_STATE = 6,  // for bluetooth sink
     BT_HEADSET_NREC = 7,
     BT_WBS = 8,
-    MMI = 9,
-    PERF_INFO = 10,
+    PERF_INFO = 201,
+    MMI = 301,
     USB_DEVICE = 101, // Check USB device type ARM or HIFI
     PARAM_KEY_LOWPOWER = 1000,
 };
