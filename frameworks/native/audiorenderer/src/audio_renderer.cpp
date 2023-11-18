f084e17b6d1d8a6a201e91f7bb0a14df509e1f5e
kw <wangyuqi30@huawei.com>
2023-11-18 18:55:46 +0800

    Revised based on review feedback
    Signed-off-by: kw <wangyuqi30@huawei.com>

--- frameworks/native/audiorenderer/src/audio_renderer.cpp
+++ frameworks/native/audiorenderer/src/audio_renderer.cpp
@@ -1216,20 +1216,27 @@ void AudioRendererStateChangeCallbackImpl::OnRendererStateChange(
     std::shared_ptr<AudioRendererDeviceChangeCallback> cb = callback_.lock();
     AUDIO_INFO_LOG("AudioRendererStateChangeCallbackImpl OnRendererStateChange");
     DeviceInfo deviceInfo = {};
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (renderer_ == nullptr) {
-        return;
-    }
-
-    if (renderer_->IsDeviceChanged(deviceInfo)) {
-        if (deviceInfo.deviceType != DEVICE_TYPE_NONE && deviceInfo.deviceType != DEVICE_TYPE_INVALID) {
-            // switch audio channel
-            renderer_->SwitchStream(deviceInfo.isLowLatencyDevice);
-        }
-        if (cb == nullptr) {
-            AUDIO_ERR_LOG("AudioRendererStateChangeCallbackImpl::OnStateChange cb == nullptr.");
+    bool isDevicedChanged = false;
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (renderer_ == nullptr) {
             return;
         }
+    
+        isDevicedChanged = renderer_->IsDeviceChanged(deviceInfo);
+        if (isDevicedChanged) {
+            if (deviceInfo.deviceType != DEVICE_TYPE_NONE && deviceInfo.deviceType != DEVICE_TYPE_INVALID) {
+                // switch audio channel
+                renderer_->SwitchStream(deviceInfo.isLowLatencyDevice);
+            }
+            if (cb == nullptr) {
+                AUDIO_ERR_LOG("AudioRendererStateChangeCallbackImpl::OnStateChange cb == nullptr.");
+                return;
+            }
+        }
+    }
+
+    if (isDevicedChanged) {
         cb->OnStateChange(deviceInfo);
     }
 }
