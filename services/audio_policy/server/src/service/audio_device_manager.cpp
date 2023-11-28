6d3a8f788b85578f862021a8abe6bb3aa632e6c0
huyue57 <huyue57@huawei.com>
2023-11-28 10:24:56 +0800

    修复分布式组网时本端播放和录制默认使用远端speaker和mic
    Signed-off-by: huyue57 <huyue57@huawei.com>
    Change-Id: Iffbd5f149fc66c13fb2a70e4e26c47d7df153a0c

--- services/audio_policy/server/src/service/audio_device_manager.cpp
+++ services/audio_policy/server/src/service/audio_device_manager.cpp
@@ -120,7 +120,7 @@ void AudioDeviceManager::MakePairedDeviceDescriptor(const shared_ptr<AudioDevice
     DeviceRole devRole)
 {
     auto isPresent = [&devDesc, &devRole] (const shared_ptr<AudioDeviceDescriptor> &desc) {
-        if (desc->deviceRole_ != devRole) {
+        if (desc->networkId_ != devDesc->networkId_ || desc->deviceRole_ != devRole) {
             return false;
         }
         if (devDesc->macAddress_ != "" && devDesc->macAddress_ == desc->macAddress_) {
@@ -286,14 +286,16 @@ void AudioDeviceManager::AddNewDevice(const sptr<AudioDeviceDescriptor> &deviceD
     UpdateDeviceInfo(devDesc);
     AddConnectedDevices(devDesc);
 
-    HandleScoWithDefaultCategory(devDesc);
-    AddDefaultDevices(deviceDescriptor);
-    AddRemoteRenderDev(devDesc);
-    AddRemoteCaptureDev(devDesc);
-
-    AddCommunicationDevices(devDesc);
-    AddMediaDevices(devDesc);
-    AddCaptureDevices(devDesc);
+    if (devDesc->networkId_ != LOCAL_NETWORK_ID) {
+        AddRemoteRenderDev(devDesc);
+        AddRemoteCaptureDev(devDesc);
+    } else {
+        HandleScoWithDefaultCategory(devDesc);
+        AddDefaultDevices(deviceDescriptor);
+        AddCommunicationDevices(devDesc);
+        AddMediaDevices(devDesc);
+        AddCaptureDevices(devDesc);
+    }
 }
 
 void AudioDeviceManager::RemoveMatchDeviceInArray(const AudioDeviceDescriptor &devDesc, string logName,
@@ -301,7 +303,8 @@ void AudioDeviceManager::RemoveMatchDeviceInArray(const AudioDeviceDescriptor &d
 {
     auto isPresent = [&devDesc] (const shared_ptr<AudioDeviceDescriptor> &desc) {
         CHECK_AND_RETURN_RET_LOG(desc != nullptr, false, "Invalid device descriptor");
-        return devDesc.deviceType_ == desc->deviceType_ && devDesc.macAddress_ == desc->macAddress_;
+        return devDesc.deviceType_ == desc->deviceType_ && devDesc.macAddress_ == desc->macAddress_ &&
+            devDesc.networkId_ == desc->networkId_;
     };
 
     auto itr = find_if(descArray.begin(), descArray.end(), isPresent);
