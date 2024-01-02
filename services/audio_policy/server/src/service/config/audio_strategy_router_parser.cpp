17c4c43b59f10f88128608502f5f54374c77b2c3
nullptr <liuyanqiu123@huawei.com>
2024-01-02 19:21:59 +0800

    修复earpiece_,speaker_,defalutMic_的pairDeviceDescriptor_为空的问题
    Description   : 修复earpiece_,speaker_,defalutMic_的pairDeviceDescriptor_为空的问题
    Sig           : SIG_MultiMedia
    Feature/Bugfix: Bugfix
    Binary Source : No
    
    Signed-off-by: nullptr <liuyanqiu123@huawei.com>

--- services/audio_policy/server/src/service/config/audio_strategy_router_parser.cpp
+++ services/audio_policy/server/src/service/config/audio_strategy_router_parser.cpp
@@ -140,6 +140,8 @@ void AudioStrategyRouterParser::AddRouters(std::vector<std::unique_ptr<RouterBas
             routers.push_back(make_unique<DefaultRouter>());
         } else if (name == "CockpitPhoneRouter") {
             routers.push_back(make_unique<CockpitPhoneRouter>());
+        } else if (name == "PairDeviceRouter") {
+            routers.push_back(make_unique<PairDeviceRouter>());
         }
     }
 }
