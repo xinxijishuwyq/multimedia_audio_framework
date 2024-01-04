601efc6a2bd3670dc149635ea2829b8b143641ba
yaojingyu2008 <yaojingyu1@huawei.com>
2024-01-04 20:02:15 +0800

    mch mixing and bug fix
    Signed-off-by: yaojingyu2008 <yaojingyu1@huawei.com>

--- frameworks/native/audioeffect/include/audio_effect_chain_adapter.h
+++ frameworks/native/audioeffect/include/audio_effect_chain_adapter.h
@@ -56,6 +56,7 @@ int32_t EffectChainManagerSetHdiParam(const char *sceneType, const char *effectM
 bool EffectChainManagerCheckA2dpOffload();
 int32_t EffectChainManagerDeleteSessionInfo(const char *sceneType, const char *sessionID);
 int32_t EffectChainManagerReturnEffectChannelInfo(const char *sceneType, uint32_t *channels, uint64_t *channelLayout);
+int32_t EffectChainManagerReturnMultiChannelInfo(uint32_t *channels, uint64_t *channelLayout);
 SessionInfoPack PackSessionInfo(const uint32_t channels, const char *channelLayout, const char *sceneMode,
     const char *spatializationEnabled);
 
