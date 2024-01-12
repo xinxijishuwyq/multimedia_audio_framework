90a1fc13b9b3254de9b0ce024a0595424fd7f43f
zhuchaochao <zhuchaochao2@huawei.com>
2024-01-12 03:54:53 +0000

    fix telephony ring bug
    Signed-off-by: zhuchaochao <zhuchaochao2@huawei.com>
    Change-Id: I5df3793ef41dc398cd6294c0a6d99b67d2f25d09

--- frameworks/native/bluetoothclient/audio_bluetooth_manager.cpp
+++ frameworks/native/bluetoothclient/audio_bluetooth_manager.cpp
@@ -15,6 +15,7 @@
 
 #include "audio_bluetooth_manager.h"
 #include "bluetooth_def.h"
+#include "ipc_skeleton.h"
 #include "audio_errors.h"
 #include "audio_log.h"
 #include "bluetooth_device_manager.h"
@@ -284,6 +285,11 @@ int32_t AudioHfpManager::ConnectScoWithAudioScene(AudioScene scene)
     std::lock_guard<std::mutex> sceneLock(g_audioSceneLock);
     int8_t lastScoCategory = GetScoCategoryFromScene(scene_);
     int8_t newScoCategory = GetScoCategoryFromScene(scene);
+    int32_t uid = IPCSkeleton::GetCallingUid();
+    if (scene == AUDIO_SCENE_RINGING && uid == TELEPHONY_UID) {
+        AUDIO_INFO_LOG("Telephony call ring scene need set correct category.");
+        newScoCategory = ScoCategory::SCO_CALLULAR;
+    }
     if (lastScoCategory == newScoCategory) {
         AUDIO_DEBUG_LOG("AudioScene category is not changed, ignore ConnectScoWithAudioScene operation.");
         return SUCCESS;
@@ -309,6 +315,9 @@ int32_t AudioHfpManager::ConnectScoWithAudioScene(AudioScene scene)
         CHECK_AND_RETURN_RET_LOG(ret == 0, ERROR, "ConnectScoWithAudioScene failed, result: %{public}d", ret);
     }
     scene_ = scene;
+    if (newScoCategory == ScoCategory::SCO_CALLULAR) {
+        scene_ = AUDIO_SCENE_PHONE_CALL;
+    }
     return SUCCESS;
 }
 
