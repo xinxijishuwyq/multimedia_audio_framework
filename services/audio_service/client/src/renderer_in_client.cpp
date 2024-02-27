b8357d5fbefcef6af987d80c7f654d20a9322752
申廷光 <shentingguang@h-partners.com>
2024-02-27 12:17:33 +0000

    fix_ut audiorenderer
    Signed-off-by: 申廷光 <shentingguang@h-partners.com>

--- services/audio_service/client/src/renderer_in_client.cpp
+++ services/audio_service/client/src/renderer_in_client.cpp
@@ -1554,7 +1554,7 @@ bool RendererInClientInner::ReleaseAudioStream(bool releaseRunner)
 bool RendererInClientInner::FlushAudioStream()
 {
     Trace trace("RendererInClientInner::FlushAudioStream " + std::to_string(sessionId_));
-    std::unique_lock<std::mutex> statusLock(statusMutex_);
+    std::lock_guard<std::mutex>lock(writeMutex_);
     if ((state_ != RUNNING) && (state_ != PAUSED) && (state_ != STOPPED)) {
         AUDIO_ERR_LOG("Flush failed. Illegal state:%{public}u", state_.load());
         return false;
@@ -1575,8 +1575,10 @@ bool RendererInClientInner::FlushAudioStream()
     if (notifiedOperation_ != FLUSH_STREAM || notifiedResult_ != SUCCESS) {
         AUDIO_ERR_LOG("Flush failed: %{public}s Operation:%{public}d result:%{public}" PRId64".",
             (!stopWaiting ? "timeout" : "no timeout"), notifiedOperation_, notifiedResult_);
+        notifiedOperation_ = MAX_OPERATION_CODE;
         return false;
     }
+    notifiedOperation_ = MAX_OPERATION_CODE;
     waitLock.unlock();
     AUDIO_INFO_LOG("Flush stream SUCCESS, sessionId: %{public}d", sessionId_);
     return true;
@@ -1643,6 +1645,7 @@ bool RendererInClientInner::DrainAudioStream()
     if (notifiedOperation_ != DRAIN_STREAM || notifiedResult_ != SUCCESS) {
         AUDIO_ERR_LOG("Drain failed: %{public}s Operation:%{public}d result:%{public}" PRId64".",
             (!stopWaiting ? "timeout" : "no timeout"), notifiedOperation_, notifiedResult_);
+        notifiedOperation_ = MAX_OPERATION_CODE;
         return false;
     }
     notifiedOperation_ = MAX_OPERATION_CODE;
@@ -1686,8 +1689,9 @@ int32_t RendererInClientInner::Write(uint8_t *pcmBuffer, size_t pcmBufferSize, u
 
 int32_t RendererInClientInner::Write(uint8_t *buffer, size_t bufferSize)
 {
+    CHECK_AND_RETURN_RET_LOG(renderMode_ != RENDER_MODE_CALLBACK, "Write with callback is not supported");
     Trace trace("RendererInClient::Write " + std::to_string(bufferSize));
-    CHECK_AND_RETURN_RET_LOG(buffer != nullptr && bufferSize < MAX_WRITE_SIZE, ERR_INVALID_PARAM,
+    CHECK_AND_RETURN_RET_LOG(buffer != nullptr && bufferSize < MAX_WRITE_SIZE && bufferSize > 0, ERR_INVALID_PARAM,
         "invalid size is %{public}zu", bufferSize);
 
     std::lock_guard<std::mutex> lock(writeMutex_);
