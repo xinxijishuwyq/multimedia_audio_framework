5119023355d599b2529690bb3d723e53d74a94c0
hezhiqiang19@huawei.com <hezhiqiang19@huawei.com>
2024-03-01 18:28:49 +0800

    call stop when timeout|dfx for runninglock
    Signed-off-by: hezhiqiang19@huawei.com <hezhiqiang19@huawei.com>
    Change-Id: I7a07087a46a07c85b7aa67bfb1417a8430e3ef16

--- frameworks/native/hdiadapter/sink/bluetooth/bluetooth_renderer_sink.cpp
+++ frameworks/native/hdiadapter/sink/bluetooth/bluetooth_renderer_sink.cpp
@@ -450,8 +450,8 @@ int32_t BluetoothRendererSinkInner::Start(void)
     }
 
     if (keepRunningLock_ != nullptr) {
-        AUDIO_INFO_LOG("keepRunningLock lock");
-        keepRunningLock_->Lock(RUNNINGLOCK_LOCK_TIMEOUTMS_LASTING); // -1 for lasting.
+        AUDIO_INFO_LOG("keepRunningLock lock result: %{public}d",
+            keepRunningLock_->Lock(RUNNINGLOCK_LOCK_TIMEOUTMS_LASTING)); // -1 for lasting.
     } else {
         AUDIO_ERR_LOG("keepRunningLock is null, playback can not work well!");
     }
