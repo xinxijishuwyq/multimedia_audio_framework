e9a496d2c2844bffc341253a19bb780ee9e9277c
nullptr <liuyanqiu123@huawei.com>
2024-03-04 15:03:54 +0800

    netlink用法整改
    Description   : netlink用法整改
    Sig           : SIG_MultiMedia
    Feature/Bugfix: Feature
    Binary Source : No
    
    Signed-off-by: nullptr <liuyanqiu123@huawei.com>

--- services/audio_policy/server/src/service/manager/pnp_server/audio_pnp_server.cpp
+++ services/audio_policy/server/src/service/manager/pnp_server/audio_pnp_server.cpp
@@ -147,7 +147,7 @@ void AudioPnpServer::OpenAndReadWithSocket()
             continue;
         }
 
-        if (((uint32_t)fd.revents & POLLIN) == POLLIN) {
+        if (((uint32_t)fd.revents & (POLLIN | POLLERR)) != 0) {
             memset_s(&msg, sizeof(msg), 0, sizeof(msg));
             rcvLen = AudioSocketThread::AudioPnpReadUeventMsg(socketFd, msg, UEVENT_MSG_LEN);
             if (rcvLen <= 0) {
@@ -160,8 +160,6 @@ void AudioPnpServer::OpenAndReadWithSocket()
             eventInfo_ = GetAudioEventInfo(AudioSocketThread::audioSocketEvent_);
             CHECK_AND_RETURN_LOG(!eventInfo_.empty(), "invalid socket info");
             OnPnpDeviceStatusChanged(eventInfo_);
-        } else if (((uint32_t)fd.revents & POLLERR) == POLLERR) {
-            AUDIO_ERR_LOG("audio event poll error");
         }
     }
     close(socketFd);
