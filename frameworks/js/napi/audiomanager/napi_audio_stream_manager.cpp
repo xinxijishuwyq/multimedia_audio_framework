8d29096ecc8da2a3bffbf24acb3d05769f1c6dcf
yaojingyu2008 <yaojingyu1@huawei.com>
2024-03-07 14:25:12 +0800

    fix GetEffectInfoArray errcode bug
    Signed-off-by: yaojingyu2008 <yaojingyu1@huawei.com>

--- frameworks/js/napi/audiomanager/napi_audio_stream_manager.cpp
+++ frameworks/js/napi/audiomanager/napi_audio_stream_manager.cpp
@@ -385,13 +385,15 @@ napi_value NapiAudioStreamMgr::GetEffectInfoArray(napi_env env, napi_callback_in
     }
 
     auto inputParser = [env, context](size_t argc, napi_value *argv) {
-        NAPI_CHECK_ARGS_RETURN_VOID(context, argc >= ARGS_ONE, "invalid arguments", NAPI_ERR_INVALID_PARAM);
+        NAPI_CHECK_ARGS_RETURN_VOID(context, argc >= ARGS_ONE, "invalid arguments", NAPI_ERR_INPUT_INVALID);
+        napi_valuetype valueType = napi_undefined;
+        napi_typeof(env, argv[PARAM0], &valueType);
+        NAPI_CHECK_ARGS_RETURN_VOID(context, valueType == napi_number, "invalid valueType", NAPI_ERR_INPUT_INVALID);
         context->status = NapiParamUtils::GetValueInt32(env, context->streamUsage, argv[PARAM0]);
         NAPI_CHECK_ARGS_RETURN_VOID(context, context->status == napi_ok, "getstreamUsage failed",
             NAPI_ERR_INVALID_PARAM);
         if (!NapiAudioEnum::IsLegalInputArgumentStreamUsage(context->streamUsage)) {
-            context->SignError(context->errCode ==
-                NAPI_ERR_INVALID_PARAM? NAPI_ERR_INVALID_PARAM : NAPI_ERR_UNSUPPORTED);
+            context->SignError(context->errCode == NAPI_ERR_INVALID_PARAM);
         }
     };
     context->GetCbInfo(env, info, inputParser);
