9724db91bc4f0b503488ede7b5f676c4dbdfbe55
chongteng wang <wangchongteng@huawei.com>
2024-03-08 14:33:58 +0800

    新增空间音频播放ndk
    Signed-off-by: chongteng wang <wangchongteng@huawei.com>

--- interfaces/kits/c/audio_renderer/native_audiorenderer.h
+++ interfaces/kits/c/audio_renderer/native_audiorenderer.h
@@ -40,7 +40,7 @@
 
 #include <time.h>
 #include "native_audiostream_base.h"
-#include "native_audio_channel_layout.h"
+#include "multimedia/native_audio_channel_layout.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
