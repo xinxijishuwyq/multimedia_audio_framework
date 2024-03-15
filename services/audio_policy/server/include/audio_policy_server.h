57a820676c653ce725fd02560e514d8983f9d7ed
huang-jianfei200 <huangjianfei4@huawei.com>
2024-03-15 09:06:00 +0000

    Add prop item and parser map
    Signed-off-by: huang-jianfei200 <huangjianfei4@huawei.com>
    Change-Id: I16fb3807070dbef659a3584c47b3330ae7288750

--- services/audio_policy/server/include/audio_policy_server.h
+++ services/audio_policy/server/include/audio_policy_server.h
@@ -508,6 +508,7 @@ private:
     AudioSpatializationService& audioSpatializationService_;
     std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler_;
     bool isAvSessionSetVoipStart = false;
+    bool volumeApplyToAll_ = false;
     std::set<uint32_t> saveAppCapTokenIdThroughMS;
 };
 } // namespace AudioStandard
