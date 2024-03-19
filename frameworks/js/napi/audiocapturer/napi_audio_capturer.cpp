6331c328f1cec78928328be8362e0025740cdb25
qugr <quguiren@huawei.com>
2024-03-19 20:16:05 +0800

    update for crossplatform
    Signed-off-by: qugr <quguiren@huawei.com>

--- frameworks/js/napi/audiocapturer/napi_audio_capturer.cpp
+++ frameworks/js/napi/audiocapturer/napi_audio_capturer.cpp
@@ -13,8 +13,9 @@
  * limitations under the License.
  */
 #include "napi_audio_capturer.h"
-
+#if !defined(ANDROID_PLATFORM) && !defined(IOS_PLATFORM)
 #include "xpower_event_js.h"
+#endif
 #include "audio_errors.h"
 #include "audio_utils.h"
 #include "napi_audio_error.h"
@@ -403,7 +404,9 @@ napi_value NapiAudioCapturer::Start(napi_env env, napi_callback_info info)
     auto complete = [env](napi_value &output) {
         output = NapiParamUtils::GetUndefinedValue(env);
     };
+#if !defined(ANDROID_PLATFORM) && !defined(IOS_PLATFORM)
     HiviewDFX::ReportXPowerJsStackSysEvent(env, "STREAM_CHANGE", "SRC=Audio");
+#endif
     return NapiAsyncWork::Enqueue(env, context, "Start", executor, complete);
 }
 
