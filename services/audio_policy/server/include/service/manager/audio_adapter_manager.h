7f7a5ea51523528dc826725ff7d637172c221c90
刘乐 <liule50@huawei.com>
2024-03-25 03:34:37 +0000

    加锁防止访问被释放的内存
    Signed-off-by: 刘乐 <liule50@huawei.com>

--- services/audio_policy/server/include/service/manager/audio_adapter_manager.h
+++ services/audio_policy/server/include/service/manager/audio_adapter_manager.h
@@ -192,6 +192,10 @@ private:
     void UpdateMuteStatusForVolume(AudioStreamType streamType, int32_t volumeLevel);
     int32_t SetVolumeDb(AudioStreamType streamType);
     int32_t SetVolumeDbForVolumeTypeGroup(const std::vector<AudioStreamType> &volumeTypeGroup, float volumeDb);
+    bool GetStreamMuteInternal(AudioStreamType streamType);
+    int32_t SetRingerModeInternal(AudioRingerMode ringerMode);
+    int32_t SetStreamMuteInternal(AudioStreamType streamType, bool mute);
+    void InitKVStoreInternal();
 
     template<typename T>
     std::vector<uint8_t> TransferTypeToByteArray(const T &t)
@@ -216,6 +220,7 @@ private:
     std::unordered_map<AudioStreamType, int> minVolumeIndexMap_;
     std::unordered_map<AudioStreamType, int> maxVolumeIndexMap_;
     std::mutex systemSoundMutex_;
+    std::mutex muteStatusMutex_;
     std::unordered_map<std::string, std::string> systemSoundUriMap_;
     StreamVolumeInfoMap streamVolumeInfos_;
     std::unordered_map<AudioStreamType, bool> muteStatusMap_;
