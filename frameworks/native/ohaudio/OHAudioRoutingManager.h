89484666c89f0038ea612ee55ee6bd7f726255bc
zhaijunwei <zhaijunwei123@huawei.com>
2024-04-11 23:47:30 +0800

    新增NDK设备监听接口
    Signed-off-by: zhaijunwei <zhaijunwei123@huawei.com>

---
+++ frameworks/native/ohaudio/OHAudioRoutingManager.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OH_AUDIO_ROUTING_MANAGER_H
+#define OH_AUDIO_ROUTING_MANAGER_H
+
+#include "native_audioroutingmanager.h"
+#include "native_audiocommon_base.h"
+#include "native_audiodevice_base.h"
+#include "audio_system_manager.h"
+#include "OHAudioDeviceDescriptor.h"
+#include "audio_info.h"
+#include "audio_log.h"
+
+namespace OHOS {
+namespace AudioStandard {
+
+class OHAudioOnDeviceChangedCallback : public AudioManagerDeviceChangeCallback {
+public:
+    OHAudioOnDeviceChangedCallback(OH_AudioRoutingManager_OnDeviceChangedCallback callback) : callback_(callback)
+    {
+    }
+    void OnDeviceChange(const DeviceChangeAction &deviceChangeAction) override;
+
+    OH_AudioRoutingManager_OnDeviceChangedCallback GetCallback()
+    {
+        return callback_;
+    }
+
+    ~OHAudioOnDeviceChangedCallback() {
+        AUDIO_INFO_LOG("~OHAudioOnDeviceChangedCallback called.\n");
+        if (callback_ != nullptr) {
+            callback_ = nullptr;
+        }
+    }
+
+private:
+    OH_AudioRoutingManager_OnDeviceChangedCallback callback_;
+};
+
+class OHAudioRoutingManager {
+public:
+    OHAudioRoutingManager();
+    ~OHAudioRoutingManager();
+
+    static OHAudioRoutingManager* GetInstance() {
+        if (!ohAudioRoutingManager_) {
+            ohAudioRoutingManager_ = new OHAudioRoutingManager();
+        }
+        return ohAudioRoutingManager_;
+    }
+    OH_AudioDeviceDescriptorArray* GetDevices(DeviceFlag deviceFlag);
+    OH_AudioCommon_Result SetDeviceChangeCallback(const DeviceFlag flag,
+        OH_AudioRoutingManager_OnDeviceChangedCallback callback);
+    OH_AudioCommon_Result UnsetDeviceChangeCallback(DeviceFlag flag,
+        OH_AudioRoutingManager_OnDeviceChangedCallback ohOnDeviceChangedcallback);
+
+private:
+    static OHAudioRoutingManager *ohAudioRoutingManager_;
+    AudioSystemManager* audioSystemManager_ = AudioSystemManager::GetInstance();
+    std::vector<std::shared_ptr<OHAudioOnDeviceChangedCallback>> ohAudioOnDeviceChangedCallbackArray_;
+};
+OHAudioRoutingManager* OHAudioRoutingManager::ohAudioRoutingManager_ = nullptr;
+
+} // namespace AudioStandard
+} // namespace OHOS
+#endif // OH_AUDIO_ROUTING_MANAGER_H
