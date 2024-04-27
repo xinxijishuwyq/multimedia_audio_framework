4bcd398e94293f75f7922e31bee3845ae3a27c39
wangtao <wangtao570@huawei.com>
2024-04-27 07:53:32 +0000

    fix no abs bt vol
    Signed-off-by: wangtao <wangtao570@huawei.com>
    Change-Id: I0b46077d62c508c0df41b8fa7def6ba3179eb539

--- services/audio_policy/server/src/service/manager/audio_adapter_manager.cpp
+++ services/audio_policy/server/src/service/manager/audio_adapter_manager.cpp
@@ -1650,6 +1650,11 @@ void AudioAdapterManager::GetStreamVolumeInfoMap(StreamVolumeInfoMap &streamVolu
     streamVolumeInfos = streamVolumeInfos_;
 }
 
+void AudioAdapterManager::SetActiveDevice(DeviceType deviceType)
+{
+    currentActiveDevice_ = deviceType;
+}
+
 DeviceType AudioAdapterManager::GetActiveDevice()
 {
     return currentActiveDevice_;
