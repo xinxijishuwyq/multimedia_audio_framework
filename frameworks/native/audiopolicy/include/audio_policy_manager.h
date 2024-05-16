f146bea18c02a67cd12c1cba4a75e264c2d18986
kw <wangyuqi30@huawei.com>
2024-05-16 10:30:51 +0800

    Fix: Attempt to resolve rare client callback reception issue
    Signed-off-by: kw <wangyuqi30@huawei.com>

--- frameworks/native/audiopolicy/include/audio_policy_manager.h
+++ frameworks/native/audiopolicy/include/audio_policy_manager.h
@@ -374,6 +374,8 @@ private:
     std::mutex stateChangelistenerStubMutex_;
     std::mutex clientTrackerStubMutex_;
     sptr<AudioPolicyClientStubImpl> audioPolicyClientStubCB_;
+    std::atomic<bool> isAudioPolicyClientRegisted_ = false;
+
     static std::unordered_map<int32_t, std::weak_ptr<AudioRendererPolicyServiceDiedCallback>> rendererCBMap_;
     static sptr<AudioPolicyClientStubImpl> audioStaticPolicyClientStubCB_;
     static std::vector<std::shared_ptr<AudioStreamPolicyServiceDiedCallback>> audioStreamCBMap_;
