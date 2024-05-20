e9d85809a41cffae2659ff2135e29d64c427cfb0
张欢 <zhanghuan182@h-partners.com>
2024-05-20 09:32:46 +0000

    update frameworks/native/audioutils/src/audio_utils.cpp.
    Signed-off-by: 张欢 <zhanghuan182@h-partners.com>

--- frameworks/native/audioutils/src/audio_utils.cpp
+++ frameworks/native/audioutils/src/audio_utils.cpp
@@ -755,7 +755,7 @@ bool AudioLatencyMeasurement::MockPcmData(uint8_t *buffer, size_t bufferLen)
     memset_s(buffer, bufferLen, 0, bufferLen);
     int16_t *signal = signalData_.get();
     size_t newlyMocked = bufferLen * MILLISECOND_PER_SECOND /
-        (uint32_t)(channelCount_ * sampleRate_ * formatByteSize_);
+        static_cast<uint32_t>(channelCount_ * sampleRate_ * formatByteSize_);
     mockedTime_ += newlyMocked;
     if (mockedTime_ >= MOCK_INTERVAL) {
         mockedTime_ = 0;
