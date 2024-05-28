594a4bc2a546e4c926e0c8430b237bbce973f78d
caoshuai <caoshuai12@h-partners.com>
2024-05-28 21:13:50 +0800

    编码规范修改
    Signed-off-by: caoshuai <caoshuai12@h-partners.com>
    Change-Id: I05f23a1d811bf86c6bdfdc643272afbd545be772

--- frameworks/native/hdiadapter/sink/primary/audio_renderer_sink.cpp
+++ frameworks/native/hdiadapter/sink/primary/audio_renderer_sink.cpp
@@ -1266,7 +1266,11 @@ int32_t AudioRendererSinkInner::UpdateDPAttrs(const std::string &usbInfoStr)
     if (attr_.channel <= 0 || attr_.sampleRate <= 0) {
         AUDIO_ERR_LOG("check attr failed channel[%{public}d] sampleRate[%{public}d]", attr_.channel, attr_.sampleRate);
     }
-    uint32_t formatByte = stoi(bufferSize) * BUFFER_CALC_1000MS / BUFFER_CALC_20MS / attr_.channel / attr_.sampleRate;
+    uint32_t formatByte = 0;
+    if (attr_.channel != 0 && attr_.sampleRate != 0) {
+        formatByte = stoi(bufferSize) * BUFFER_CALC_1000MS / BUFFER_CALC_20MS / attr_.channel / attr_.sampleRate;
+    }
+    
     attr_.format = static_cast<HdiAdapterFormat>(ConvertByteToAudioFormat(formatByte));
 
     AUDIO_DEBUG_LOG("UpdateDPAttrs sampleRate %{public}d, format: %{public}d,channelCount: %{public}d",
