1dd7198d82c5498696c67c003372eed38246880a
chuxixue <chuxixue@huawei.com>
2024-07-25 13:35:11 +0800

    门禁告警
    Signed-off-by: chuxixue <chuxixue@huawei.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -4836,13 +4836,15 @@ int32_t AudioPolicyService::UpdateTracker(AudioMode &mode, AudioStreamChangeInfo
     if (rendererState == RENDERER_PREPARED || rendererState == RENDERER_NEW || rendererState == RENDERER_INVALID) {
         return ret; // only update tracker in new and prepared
     }
+    
+    if (rendererState == RENDERER_RELEASED) {
+        if (!streamCollector_.ExistStreamForPipe(PIPE_TYPE_MULTICHANNEL)) {
+            DynamicUnloadModule(PIPE_TYPE_MULTICHANNEL);
+        }
 
-    if (rendererState == RENDERER_RELEASED && !streamCollector_.ExistStreamForPipe(PIPE_TYPE_MULTICHANNEL)) {
-        DynamicUnloadModule(PIPE_TYPE_MULTICHANNEL);
-    }
-
-    if (rendererState == RENDERER_RELEASED && !streamCollector_.ExistStreamForPipe(PIPE_TYPE_OFFLOAD)) {
-        DynamicUnloadModule(PIPE_TYPE_OFFLOAD);
+        if (!streamCollector_.ExistStreamForPipe(PIPE_TYPE_OFFLOAD)) {
+            DynamicUnloadModule(PIPE_TYPE_OFFLOAD);
+        }
     }
 
     if (mode == AUDIO_MODE_PLAYBACK && (rendererState == RENDERER_STOPPED || rendererState == RENDERER_PAUSED)) {
