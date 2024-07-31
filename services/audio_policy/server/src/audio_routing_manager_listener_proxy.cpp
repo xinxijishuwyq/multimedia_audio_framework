abd524398bdcaf89f05a85e3c696099b06d0049c
WangWanliang <wangwanliang1@h-partners.com>
2024-07-31 18:01:25 +0800

    音频冗余头文件整改
    Signed-off-by: WangWanliang <wangwanliang1@h-partners.com>

--- services/audio_policy/server/src/audio_routing_manager_listener_proxy.cpp
+++ services/audio_policy/server/src/audio_routing_manager_listener_proxy.cpp
@@ -15,7 +15,7 @@
 #undef LOG_TAG
 #define LOG_TAG "AudioRoutingManagerListenerProxy"
 
-#include "audio_routing_manager_listener_proxy.h""
+#include "audio_routing_manager_listener_proxy.h"
 #include "audio_policy_log.h"
 #include "audio_errors.h"
 
