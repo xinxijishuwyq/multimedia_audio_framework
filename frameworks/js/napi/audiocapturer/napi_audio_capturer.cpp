c0df1a1a7a6eb9ebd83de62f50d823f0ef06fa28
heyabinghyb <heyabing@h-partners.com>
2024-08-10 11:05:54 +0000

    解决报错
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- frameworks/js/napi/audiocapturer/napi_audio_capturer.cpp
+++ frameworks/js/napi/audiocapturer/napi_audio_capturer.cpp
@@ -432,7 +432,7 @@ napi_status NapiAudioCapturer::ReadFromNative(shared_ptr<AudioCapturerAsyncConte
     CHECK_AND_RETURN_RET_LOG(buffer != nullptr, status, "buffer malloc failed,no memery");
     int32_t bytesRead = 0;
     while (static_cast<uint32_t>(bytesRead) < context->userSize) {
-        int32_t len = napiAudioCapturer->audioCapturer_->Read(*(buffer.get() + bytesRead),
+        int32_t len = napiAudioCapturer->audioCapturer_->Read(*(buffer + bytesRead),
             userSize - bytesRead, context->isBlocking);
         if (len >= 0) {
             bytesRead += len;
@@ -446,7 +446,7 @@ napi_status NapiAudioCapturer::ReadFromNative(shared_ptr<AudioCapturerAsyncConte
         return status;
     }
     context->bytesRead = static_cast<size_t>(bytesRead);
-    context->buffer = buffer.get();
+    context->buffer = buffer;
     status = napi_ok;
     return status;
 }
